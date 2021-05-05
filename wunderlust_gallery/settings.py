aa2ea649b56d30cf611ca876a506ef85dc2924a8
Jade-ux <jadeemailsandbox@gmail.com>
2021-05-05 05:50:30 +0000

    Changed allowed hosts URL for Heroku.

--- wunderlust_gallery/settings.py
+++ wunderlust_gallery/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ecommerce-test11.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ecommerce-test11.herokuapp.com', 'localhost']
 
 
 # Application definition
